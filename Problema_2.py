2a098d0a6a481efb7638c71dfd090e9c3f2cccf2
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-09-15 10:36:03 +0300

    Add files via upload

---
+++ Problema_2.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+print("suma a+b=", a+b)
+print("diferenta a-b=", a-b)
+print("catul impartirii a//b=", a//b)
+print("restul impartirii a%b=", a%b)
+print("produsul a*b=", a*b)
+print("ridicarea la putere a**b=", a**b)
