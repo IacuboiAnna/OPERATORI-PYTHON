5a4c913ae4c860d7e4eb0eacc2d034dfbae20f99
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-09-16 22:14:44 +0300

    Add files via upload

---
+++ Problema_4.py
@@ -0,0 +1,5 @@
+n=9
+print(n, "m =", (10**2)*n, "cm" )
+print(n, "kg =", (10**6)*n, "mg")
+print(n, "ani =", 12*n, "luni =", 52*n, "saptamani =", 365*n, "zile")
+#Calculam considerand ca anul are aproximativ 52 de saptamani si 365 de zile
