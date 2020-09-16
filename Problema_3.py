99e947d5136f6ddf5f1c4dce9757716cdac7fd1d
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-09-16 21:38:27 +0300

    Add files via upload

---
+++ Problema_3.py
@@ -0,0 +1,5 @@
+l=10
+#l este lungimea laturii cubului in cm
+print("Perimetrul suprafetei cubului este", 12*l, "cm")
+print("Aria tuturor suprafetelor cubului este", 6*(l**2), "cm2")
+print("Volumul cubului este", l**3, "cm3")
