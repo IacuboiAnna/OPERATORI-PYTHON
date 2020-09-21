606996a178f27ab45dc11577404b2cc60ac72aa2
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-09-21 21:05:50 +0300

    Add files via upload

---
+++ Problema_5.py
@@ -0,0 +1,13 @@
+print("x      y      x and y x or y  not x   not y"  )
+x=True
+y=False
+print(x, y, x and y, x or y, not x, not y, sep='   ')
+x=False
+y=True
+print(x, y, x and y, x or y, not x, not y, sep='   ')
+x=True
+y=True
+print(x, y, x and y, x or y, not x, not y, sep='    ')
+x=False
+y=False
+print(x, y, x and y, x or y, not x, not y, sep='   ')
